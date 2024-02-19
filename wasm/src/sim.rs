c4ea724b440123a2551e501c1bb95129c4a4f07b
Ben Barber <benbarber121@gmail.com>
2024-02-19 00:18:41 -0500

    impl particle life wasm

--- wasm/src/sim.rs
+++ wasm/src/sim.rs
@@ -1,66 +1,207 @@
 #![allow(unused)]
-use rand::Rng;
+use na::{DMatrix, Point2, SimdPartialOrd, Vector2};
+use nalgebra as na;
+use rand::{
+    distributions::{Distribution, Uniform},
+    Rng,
+};
+use serde::{ser::SerializeSeq, Serialize};
+use serde_json::Value;
+use std::{
+    collections::HashMap,
+    sync::{Arc, RwLock},
+    thread,
+};
+use wasm_bindgen::prelude::wasm_bindgen;
 
-pub struct Vector2D {
-    pub x: f32,
-    pub y: f32,
-}
-
-impl Vector2D {
-    pub fn new(x: f32, y: f32) -> Self {
-        Self { x, y }
-    }
-}
-
-pub struct Particle {
-    pub x: u32,
-    pub y: u32,
-    pub vx: u32,
-    pub vy: u32,
+#[derive(Debug)]
+struct Particle {
+    pos: Point2<f64>,
+    vel: Vector2<f64>,
 }
 
 impl Particle {
-    pub fn new(world_size: u32) -> Self {
+    fn new(world_size: u32) -> Self {
         let mut rng = rand::thread_rng();
         Self {
-            x: rng.gen_range(0..world_size),
-            y: rng.gen_range(0..world_size),
-            vx: 0,
-            vy: 0,
+            pos: na::point![
+                rng.gen_range(0..world_size) as f64,
+                rng.gen_range(0..world_size) as f64,
+            ],
+            vel: na::vector![0., 0.],
         }
     }
 
     /// Get the force another particle exerts on this particle given the gravitational constant g.
-    pub fn force(&self, other: &Particle, g: f32) -> Vector2D {
-        let dx = (self.x - other.x) as f32;
-        let dy = (self.y - other.y) as f32;
-        let d = dx.hypot(dy);
-        if d > 0. && d < 80. {
-            let fx = g * (1./dx);
-            let fy = g * (1./dy);
-            Vector2D::new(fx, fy)
+    fn force(&self, other: &Particle, g: f64) -> Vector2<f64> {
+        let d = (self.pos - other.pos).abs();
+        let md = d.magnitude();
+        if md > 0. && md < 80. {
+            d.map(|c| g * (1. / c))
         } else {
-            Vector2D::new(0., 0.)
+            na::vector![0., 0.]
         }
     }
 }
 
-pub struct Culture {
-    pub color: String,
-    pub particles: Vec<Particle>,
+impl Serialize for Particle {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        let mut seq = serializer.serialize_seq(Some(self.pos.len()))?;
+        for e in self.pos.iter() {
+            seq.serialize_element(e)?;
+        }
+        seq.end()
+    }
+}
+
+#[derive(Debug, Serialize)]
+struct Culture {
+    color: String,
+    particles: Vec<Particle>,
 }
 
 impl Culture {
-    pub fn new(color: &str, world_size: u32, population: usize) -> Self {
+    fn new(color: String, world_size: u32, population: usize) -> Self {
         let particles = std::iter::repeat_with(|| Particle::new(world_size))
             .take(population)
             .collect::<Vec<_>>();
 
+        Self { color, particles }
+    }
+
+    fn force(&self, other: &Culture, g: f64) -> Vec<Vector2<f64>> {
+        self.particles
+            .iter()
+            .map(|p1| {
+                // Accumulate force on p1
+                other
+                    .particles
+                    .iter()
+                    .fold(na::vector![0., 0.], |acc, p2| acc + p1.force(p2, g))
+            })
+            .collect()
+    }
+}
+
+#[derive(Debug, Serialize)]
+#[wasm_bindgen]
+pub struct PetriDish {
+    world_size: u32,
+    population_per_culture: usize,
+    cultures: Vec<Culture>,
+    #[serde(skip)]
+    gravity_mesh: Vec<Vec<f64>>,
+}
+
+#[wasm_bindgen]
+impl PetriDish {
+    #[wasm_bindgen(constructor)]
+    pub fn new(colors: Vec<String>, world_size: u32, population: usize) -> Self {
+        let cultures = colors
+            .into_iter()
+            .map(|color| Culture::new(color.to_string(), world_size, population))
+            .collect::<Vec<_>>();
+        let num_cultures = cultures.len();
+        let mut rng = rand::thread_rng();
+        let distr = Uniform::new_inclusive(-1., 1.);
+        let gravity_mesh = (0..num_cultures)
+            .map(|_| distr.sample_iter(&mut rng).take(num_cultures).collect())
+            .collect();
         Self {
-            color: color.to_owned(),
-            particles,
+            world_size,
+            population_per_culture: population,
+            cultures,
+            gravity_mesh,
         }
     }
 
-    
+    // pub fn step(&mut self) {
+    //     let arc_cultures = self
+    //         .cultures
+    //         .iter()
+    //         .map(|c| Arc::new(RwLock::new(c)))
+    //         .collect::<Vec<_>>();
+    //     let arc_gravity = self
+    //         .gravity_mesh
+    //         .iter()
+    //         .map(|g| Arc::new(RwLock::new(g)))
+    //         .collect::<Vec<_>>();
+    //     let mut handles = Vec::with_capacity(self.cultures.len());
+    //     for i in 0..self.cultures.len() {
+    //         let cultures = arc_cultures.iter().map(Arc::clone).collect::<Vec<_>>();
+    //         let gravity_vecs = arc_gravity.iter().map(Arc::clone).collect::<Vec<_>>();
+    //         let handle = thread::spawn(move || {
+    //             let c1 = cultures[i].read().unwrap();
+    //             let gravity_vec = gravity_vecs[i].read().unwrap();
+    //             for j in 0..cultures.len() {
+    //                 let c2 = cultures[j].read().unwrap();
+    //                 c1.force(&c2, gravity_vec[j]);
+    //             }
+    //         });
+    //         handles.push(handle);
+    //     }
+    //     for handle in handles {
+    //         handle.join();
+    //     }
+    //     ()
+    // }
+
+    pub fn step(&mut self) {
+        // Generate force tensor
+        let force_tensor: Vec<Vec<Vector2<f64>>> = self
+            .cultures
+            .iter()
+            .enumerate()
+            .map(|(i, c1)| {
+                let initial_forces = vec![na::vector![0., 0.]; 20];
+                self.cultures
+                    .iter()
+                    .enumerate()
+                    .fold(initial_forces, |acc, (j, c2)| {
+                        let forces = c1.force(c2, self.gravity_mesh[i][j]);
+                        acc.into_iter()
+                            .zip(forces)
+                            .map(|(f1, f2)| f1 + f2)
+                            .collect()
+                    })
+            })
+            .collect();
+
+        // Apply force tensor
+        for (i, culture) in self.cultures.iter_mut().enumerate() {
+            for (j, p) in culture.particles.iter_mut().enumerate() {
+                let force = force_tensor[i][j];
+                p.vel = (p.vel + force) * 0.5;
+                p.pos += p.vel;
+                if p.pos.x <= 0. || p.pos.x >= self.world_size as f64 {
+                    p.vel.x *= -1.;
+                }
+                if p.pos.y <= 0. || p.pos.y >= self.world_size as f64 {
+                    p.vel.y *= -1.;
+                }
+            }
+        }
+    }
+
+    pub fn gravity_mesh(&self) -> String {
+        serde_json::to_string(&self.gravity_mesh).unwrap()
+    }
+}
+
+#[test]
+fn test() {
+    let mut pd = PetriDish::new(
+        vec!["red".to_string(), "blue".to_string(), "green".to_string()],
+        200,
+        1,
+    );
+    println!("Before: {:#}", serde_json::to_value(&pd).unwrap());
+    for i in 0..100 {
+        pd.step()
+    }
+    println!("After: {:#}", serde_json::to_value(&pd).unwrap());
+    println!("Gravity Mesh: {:#}", pd.gravity_mesh())
 }
